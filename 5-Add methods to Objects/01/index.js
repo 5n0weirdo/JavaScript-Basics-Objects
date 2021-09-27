67946ed765b9f0531616d6cc92d615e7e4ebc4d6
5n0weirdo <snoweirdo@protonmail.com>
2021-09-27 16:19:17 +0200

    Add Methods to Objects

---
+++ 5-Add methods to Objects/01/index.js
@@ -0,0 +1,10 @@
+var webdev = {
+  language: "Python",
+  position: "senior",
+  experience: ["Designer", "Web Developer"],
+  //added a method:
+  add: function(x, y) {
+    return x + y;
+  }
+};
+console.log(webdev.add(10,5));
