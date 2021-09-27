58981ab51f4cf42ffed2b1faa163759c5307fdb8
5n0weirdo <snoweirdo@protonmail.com>
2021-09-27 15:55:17 +0200

    Arrays vs Objects

---
+++ 2-Arrays VS Objects/01/index.js
@@ -0,0 +1,17 @@
+// Array:
+var names = ["Design", "web", "dev"];
+console.log(names[1]); //"web"
+// change index 1 value:
+names[1] = "WebDev";
+console.log(names[1]);
+
+// Object:
+var developer = {
+  name: "dev",
+  language: "Go"
+}
+console.log(developer.name);
+
+// change language value:
+developer.language = "Python";
+console.log(developer.language);
